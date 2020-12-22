be3e8e2deaf22e2c404f2813b2703eecdc799d2a
smallbee3 <smallbee3@gmail.com>
2020-12-22 16:08:08 +0900

    Refactor webpack.config.js to receive 'NODE_ENV' parameter

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,48 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
-  output: {
-    publicPath: '/',
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+module.exports = ({ NODE_ENV }) => {
+  return {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+      publicPath: '/',
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: path.resolve(__dirname, 'tsconfig.json'),
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
     plugins: [
-      new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, 'tsconfig.json'),
+      new CleanWebpackPlugin(),
+      new HTMLWebpackPlugin({
+        filename: 'index.html',
+        title: 'NetwrokDefines - Video & Network Technology Company',
+        favicon: 'public/favicon.ico',
+        template: path.resolve(__dirname, './public/index.html'),
       }),
     ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      filename: 'index.html',
-      title: 'NetwrokDefines - Video & Network Technology Company',
-      favicon: 'public/favicon.ico',
-      template: path.resolve(__dirname, './public/index.html'),
-    }),
-  ],
-  optimization: { minimizer: [] },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  },
-  devtool: 'inline-source-map'
+    optimization: { minimizer: [] },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+    },
+    devtool: 'inline-source-map',
+  };
 };
