ca0a25f570e7bff875f97ca216a84a8e2da0f469
smallbee3 <smallbee3@gmail.com>
2021-01-05 17:55:24 +0900

    Add GlobalStyle using style-components createGlobalStyle method

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,7 +1,18 @@
 import React, { ReactElement } from 'react';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import {
+  createGlobalStyle,
+  ThemeProvider as StyledThemeProvider,
+} from 'styled-components';
 
 import { styledTheme } from '../theme';
+
+const GlobalStyle = createGlobalStyle`
+  body, p {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, 'Noto Sans KR', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 interface Props {
   children?: ReactElement;
@@ -9,7 +20,10 @@ interface Props {
 
 function ThemeProvider({ children }: Props): ReactElement {
   return (
-    <StyledThemeProvider theme={styledTheme}>{children}</StyledThemeProvider>
+    <>
+      <GlobalStyle />
+      <StyledThemeProvider theme={styledTheme}>{children}</StyledThemeProvider>
+    </>
   );
 }
 
