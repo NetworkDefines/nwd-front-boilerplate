6e0e6494630e3d3e28139498a8c8bc0ca9a508fe
smallbee3 <smallbee3@gmail.com>
2021-01-13 18:43:32 +0900

    Add media-query breakpoints based on that of material-ui

--- src/theme.ts
+++ src/theme.ts
@@ -100,3 +100,21 @@ export const muiTheme: DefaultTheme = responsiveFontSizes(
     overrides: {},
   }),
 );
+
+const size = {
+  sMobile: 320,
+  mobile: 480,
+  tablet: 600,
+  desktop: 960,
+  largeScreen: 1280,
+  extraLargeScreen: 1920,
+};
+
+export const device = {
+  sMobile: `(min-width: ${size.sMobile}px)`,
+  mobile: `(min-width: ${size.mobile}px)`,
+  tablet: `(min-width: ${size.tablet}px)`,
+  desktop: `(min-width: ${size.desktop}px)`,
+  largeScreen: `(min-width: ${size.largeScreen}px)`,
+  extraLargeScreen: `(min-width: ${size.extraLargeScreen}px)`,
+};
